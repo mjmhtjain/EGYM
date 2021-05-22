ca2f1257752c3b296505d05ceeadc94f2d63ca8c
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2021-05-22 14:04:11 +0530

    RankingApiControllerIntegrationTest incomplete

--- src/test/java/com/egym/recruiting/codingtask/api/RankingApiControllerIntegrationTest.java
+++ src/test/java/com/egym/recruiting/codingtask/api/RankingApiControllerIntegrationTest.java
@@ -1,43 +1,65 @@
 package com.egym.recruiting.codingtask.api;
 
-import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
+import com.egym.recruiting.codingtask.Application;
+import com.egym.recruiting.codingtask.dto.ExerciseDTO;
+import com.egym.recruiting.codingtask.model.ExerciseType;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.jayway.restassured.RestAssured;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+
+import static com.jayway.restassured.RestAssured.given;
 
 /**
  * Integration test for testing the RESTful APIs provided by the {@link ExerciseApiController}. For easier testing we are going
  * to rely on the Rest Assured framework.
  */
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RankingApiControllerIntegrationTest {
+    private final String baseUrl = "http://localhost:8080";
 
-	@LocalServerPort
-	int localServerPort;
+    @Test
+    public void testGetRanking() {
+        long userId = 1;
+        //insert a bunch of entries
+        insertExercises(1, 10);
+        insertExercises(2, 10);
 
-	@BeforeEach
-	public void setUp() {
-		RestAssured.port = localServerPort;
-	}
+        //assert ranking endpoint
+        given().queryParameters(Map.of("userIds","1,2"))
+                .when().get(baseUrl + "/ranking")
+                .then().statusCode(HttpStatus.SC_OK)
+                .body("", Matchers.hasSize(2));
+    }
 
-	@Test
-	public void testGetRanking() {
-		when().get("/ranking?userIds=1&userIds=2").
-				then().statusCode(HttpStatus.SC_OK).body("", Matchers.hasSize(0));
-	}
+    void insertExercises(long userId, long noOfEntries) {
+        for (long i = 0; i < noOfEntries; i++) {
+            ExerciseDTO ex1 = getDemoExerciseObj(userId, i);
+            given().body(ex1).contentType(MediaType.APPLICATION_JSON_VALUE).
+                    when().post(baseUrl + "/exercise").
+                    then().statusCode(HttpStatus.SC_CREATED);
+        }
+    }
 
-	// TODO add more tests for the RankingApiController
+    private ExerciseDTO getDemoExerciseObj(long userId, long daysAgo) {
+        ExerciseDTO exerciseDTO = new ExerciseDTO();
+        exerciseDTO.setUserId(Long.valueOf(userId));
+        exerciseDTO.setType(ExerciseType.RUNNING);
+        exerciseDTO.setDescription("RUNNING");
+        exerciseDTO.setDuration(Long.valueOf(80));
+        exerciseDTO.setCalories(Long.valueOf(80));
+        exerciseDTO.setStartTime(OffsetDateTime.now().minus(daysAgo, ChronoUnit.DAYS));
+
+        return exerciseDTO;
+    }
+
+    // TODO add more tests for the RankingApiController
 }
